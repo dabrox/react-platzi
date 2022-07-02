e7256cfa3bc2655bf14175897f3d49faaf5cfeed
Daniel Sifontes <daniel@netprogroup.com>
2022-07-02 14:59:34 -0400

    Agregando estilos importando desde archivos css externos

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
 
 const todos = [
-  {text: 'Cortar cebolla', completed:false},
+  {text: 'Cortar cebolla', completed:true},
   {text: 'Tomar el curso de REACT', completed:false},
   {text: 'lalalalla', completed:false},
   {text: 'Llorar con la llorona', completed:false}
@@ -19,7 +19,11 @@ function App() {
       <TodoSearch/>
       <TodoList>
         {todos.map(todo => (
-          <TodoItem key={todo.text} text={todo.text}/>
+          <TodoItem
+          key={todo.text}
+          text={todo.text}
+          completed={todo.completed}
+        />
         ))}
       </TodoList>
       <CreateTodoButton/>
