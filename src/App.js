a4be3bba43123ef117d0162ab099bd1246c7a17f
Daniel Sifontes <daniel@netprogroup.com>
2022-07-10 18:02:58 -0400

    Contando y buscando TODOS 2.0

--- src/App.js
+++ src/App.js
@@ -19,6 +19,19 @@ function App() {
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length
   
+  let searchedTodos = []
+   if(!searchValue.length >=1 ){
+      searchedTodos = todos
+   }else{
+      
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase()
+      const searchText = searchValue.toLowerCase()
+      return todoText.includes(searchText)
+    })
+    
+   }
+
   return (
     <React.Fragment>
       <TodoCounter
@@ -30,7 +43,7 @@ function App() {
         setSearchValue={setSearchValue}
       />
       <TodoList>
-        {todos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem
           key={todo.text}
           text={todo.text}
